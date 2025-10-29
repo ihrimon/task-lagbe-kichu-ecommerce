8aac70bb58560bcf0c867897897dc3b12611dc06
ihrimon <ihrimon147@gmail.com>
2025-10-30 04:35:50 +0600

    backend project setup

---
+++ backend/src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import router from './app/routes';
+
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+app.use(cookieParser());
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    success: true,
+    message: 'Welcome to "Kisu Lagbe" E-commerce Shop',
+  });
+});
+
+app.use('/api/v1', router);
+
+export default app;
