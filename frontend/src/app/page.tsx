90c085d93f3148ce45013a8b6b3f07642f1dbbac
ihrimon <ihrimon147@gmail.com>
2025-10-30 05:49:50 +0600

    frontend project setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className='flex items-center justify-center text-5xl font-black text-purple-500'>Lagbe Kichu Ecommerce</h1>
+      </main>
+    </div>
+  );
+}
