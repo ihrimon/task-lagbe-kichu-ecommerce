4d644bbf27de11978c0e63876566e36a46ddb0a2
ihrimon <ihrimon147@gmail.com>
2025-11-03 12:23:39 +0600

    section: eventcard

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,6 @@
+import EventCard from '@/components/EventCard'
+import ExploreButton from '@/components/ExploreButton'
+import { events } from '@/lib/constants'
 
 const Home = () => {
   return (
@@ -6,6 +9,20 @@ const Home = () => {
         The Hub for Every Dev <br /> Event You Can't Miss
       </h1>
       <p className='text-center mt-5'>Hackathons, Meetups, and Conferences, All in One Place</p>
+
+      <ExploreButton />
+
+      <div className='mt-20 space-y-7'>
+    <h3>Featured Events</h3>
+
+    <ul className='events decoration-0'>
+      {events.map((event) => (
+        <li key={event.title}>
+          <EventCard {...event}/>
+        </li>
+      ))}
+    </ul>
+      </div>
     </section>
   )
 }
